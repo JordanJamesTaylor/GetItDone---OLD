f16fe4e246fc8609b36c9dcd86aacf46ebeb50d2
JordanJamesTaylor <jordjamestaylor@gmail.com>
2022-08-30 14:22:55 -0400

    Congigured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   
   # Route to test app coniguration
   get '/hello', to: 'application#hello_world'
+
+  # Handles any requests that come through that aren't requests for our API routes by returning the public/index.html file instead
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
